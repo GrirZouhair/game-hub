cabc0e3f7098e6a2a8be229bff34ce9dfed9b288
GRIRZouhair <zouhairgrir937@gmail.com>
2025-09-21 13:20:46 +0100

    Build responsive layout

---
+++ src/readme.tsx
@@ -0,0 +1,14 @@
+/*
+    Block:
+        -take full width if no width
+        -add line break
+        -respect padding, margin, width, height
+    Inline:
+        -do not respect width height
+        -respect padding and margin [just left and right]
+        -do not add line before
+        -allow elements before and after it
+    Inline-Block:
+        - allow elements before and after it
+        -respect padding, margin, width, height
+*/
